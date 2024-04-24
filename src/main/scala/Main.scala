ffca188ff522e2227278fc9ad968c25439b14c7b
csgn <dev.csgn@gmail.com>
2024-04-24 19:06:49 +0300

    [init]

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,91 @@
+import scala.annotation.tailrec
+
+/*
+ * Break camelCase
+ * https://www.codewars.com/kata/5208f99aee097e6552000148/train/scala
+ */
+def breakCamelCase(s: String): String =
+  @tailrec
+  def go(s: String, acc: String): String =
+    if s.isEmpty then acc
+    else
+      val h = s.head
+      val t = s.tail
+      if h.isLower then go(t, acc.concat(h.toString))
+      else go(t, acc.concat(" ".concat(h.toString)))
+
+  go(s, "")
+
+/*
+ * Are they the "same"?
+ * https://www.codewars.com/kata/550498447451fbbd7600041c/train/scala
+ */
+def comp(seq1: Seq[Int], seq2: Seq[Int]): Boolean =
+  if seq1 == null || seq2 == null then false
+  else
+    @tailrec
+    def go(s1: Seq[Int], s2: Seq[Int], acc: Boolean): Boolean =
+      if s1.isEmpty && s2.isEmpty then acc
+      else if s1.isEmpty || s2.isEmpty then false
+      else if s2.head == math.pow(s1.head, 2) then go(s1.tail, s2.tail, true)
+      else false
+
+    go(seq1.sorted, seq2.sorted, true)
+
+/*
+ * Build a pile of Cubes
+ * https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/scala
+ */
+def findNb(m: Long): Int =
+  @tailrec
+  def go(n: Int, acc: Long): Int =
+    if acc == m then n - 1
+    else if acc > m then -1
+    else go(n + 1, (acc + math.pow(n, 3).toLong).toLong)
+
+  go(1, 0)
+
+/*
+ * Highest Scoring Word
+ * https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/scala
+ */
+def high(s: String): String =
+  @tailrec
+  def go(ss: String, count: Int, acc: Int, buff: String, res: String): String =
+    if ss.isEmpty then
+      if count > acc then buff
+      else res
+    else
+      val h = ss.head
+      val t = ss.tail
+
+      if h == ' ' then
+        if count > acc then go(t, 0, count, "", buff)
+        else go(t, 0, acc, "", res)
+      else go(t, count + h.toInt - 96, acc, buff + h, res)
+
+  go(s, 0, 0, "", "")
+
+/*
+ * Split Strings
+ * https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/scala
+ */
+def splitStrings(s: String): List[String] =
+  def go(
+      ss: String,
+      count: Int,
+      buff: String,
+      acc: List[String]
+  ): List[String] =
+    if ss.isEmpty then
+      if !buff.isEmpty then acc ::: List(buff + "_")
+      else acc
+    else
+      val h = ss.head
+      val t = ss.tail
+      val nextBuff = buff + h
+
+      if count % 2 == 0 then go(t, 1, "", acc ::: List(nextBuff))
+      else go(t, count + 1, nextBuff, acc)
+
+  go(s, 1, "", List())
---
+++ src/test/scala/MySuite.scala
@@ -0,0 +1,128 @@
+import scala.util.Random
+
+class MySuite extends munit.FunSuite {
+  test("breakCamelCase") {
+    assertEquals(
+      breakCamelCase("helloWorld"),
+      "hello World",
+      "for breakCamelCase(\"helloWorld\")"
+    )
+    assertEquals(
+      breakCamelCase("camelCase"),
+      "camel Case",
+      "for breakCamelCase(\"camelCase\")"
+    )
+    assertEquals(
+      breakCamelCase("breakCamelCase"),
+      "break Camel Case",
+      "for breakCamelCase(\"breakCamelCase\")"
+    )
+  }
+
+  test("Are they the same?") {
+    val seq1 = Seq(121, 144, 19, 161, 19, 144, 19, 11)
+    val seq2 = Seq(
+      11 * 11,
+      121 * 121,
+      144 * 144,
+      19 * 19,
+      161 * 161,
+      19 * 19,
+      144 * 144,
+      19 * 19
+    )
+    assertEquals(
+      comp(seq1, seq2),
+      true,
+      s"\ncomp(${seq1}, ${seq2}) should be true"
+    )
+
+    val seq3 = List(121, 144, 19, 161, 19, 144, 19, 11)
+    val seq4 = List(231, 14641, 20736, 361, 25921, 361, 20736, 361)
+    assertEquals(
+      comp(seq3, seq4),
+      false,
+      s"\ncomp(${seq3}, ${seq4}) should be false"
+    )
+
+    val seq5 = List()
+    val seq6 = List()
+    assertEquals(
+      comp(seq5, seq6),
+      true,
+      s"\ncomp(${seq5}, ${seq6}) should be true"
+    )
+
+    val seq7 = null
+    val seq8 = List()
+    assertEquals(
+      comp(seq7, seq8),
+      false,
+      s"\ncomp(${seq7}, ${seq8}) should be false"
+    )
+
+    val seq9 = List(2, 2, 3)
+    val seq10 = List(4, 9, 9)
+    assertEquals(
+      comp(seq9, seq10),
+      false,
+      s"\ncomp(${seq9}, ${seq10}) should be false"
+    )
+
+    val seq11 = List.fill(8)(Random().nextInt(100))
+    val seq12 = seq11.map(x => x * x)
+    assertEquals(
+      comp(seq11, seq12),
+      true,
+      s"\ncomp(${seq11}, ${seq12}) should be true"
+    )
+  }
+
+  test("Build a pile of Cubes") {
+    val testCases = List(
+      (4183059834009L, 2022),
+      (24723578342962L, -1),
+      (135440716410000L, 4824),
+      (40539911473216L, 3568),
+      (26825883955641L, 3218),
+      (640032000400000000L, 40000)
+    )
+
+    testCases.foreach { case (m, expected) =>
+      assertEquals({ findNb(m) }, expected, s"\nInput:\n m = $m")
+    }
+  }
+
+  test("Highest Scoring Word") {
+    val testCases = List(
+      ("man i need a taxi up to ubud", "taxi"),
+      ("what time are we climbing up to the volcano", "volcano"),
+      ("take me to semynak", "semynak"),
+      ("for equals return earliestg", "return"),
+      ("find or finda", "finda"),
+      ("aa b", "aa"),
+      ("b aa", "b"),
+      ("bb d", "bb"),
+      ("d bb", "d"),
+      ("aaa b", "aaa")
+    )
+
+    testCases.foreach { case (m, expected) =>
+      assertEquals({ high(m) }, expected, s"\n$m should be $expected\n")
+    }
+  }
+
+  test("Split Strings") {
+    val testCases = List(
+      ("asdfadsf", List("as", "df", "ad", "sf")),
+      ("asdfads", List("as", "df", "ad", "s_")),
+      ("", List()),
+      ("x", List("x_"))
+    )
+
+    testCases.foreach { case (m, expected) =>
+      assertEquals({ splitStrings(m) }, expected, s"\n$m should be $expected\n")
+    }
+  }
+
+}
